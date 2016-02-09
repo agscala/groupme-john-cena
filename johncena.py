34dd50693aecb6702796e610e896deed76bec415
Jake Nabozny <jnaboz@gmail.com>
2016-02-09 10:56:15 -0800

    adding cena message as an object

--- johncena.py
+++ johncena.py
@@ -53,6 +53,9 @@ JOHN_CENA_ACTIVATE = "http://media3.giphy.com/media/xTiTnoHt2NwerFMsCI/200.gif"
 JOHN_CENA_SAD = "http://static.giantbomb.com/uploads/screen_kubrick/9/93998/2651842-untitled.jpg"
 JOHN_CENA_WTF = "https://whatistheexcel.com/wooobooru/_images/91fa21f02ccbe8980eae483263197965/812%20-%20john_cena%20macro%20wtf_am_i_looking_at%20wwe.png"
 
+GROUPME_API_URL = "https://api.groupme.com/v3/bots/post"
+BOT_ID = "63747737acc3dbf60d7df729fd"
+
 
 # =============================================================================
 # Classes
@@ -77,6 +80,47 @@ class RateLimit(object):
         return self.LIMIT_MINS - ((datetime.datetime.now() - self.last_sent[sender]).seconds // 60)
 
 
+class GroupmeMessage(object):
+    def __init__(self, text=None, attachments=None, bot_id=BOT_ID):
+        self.text = text
+        self.attachments = attachments
+        self.bot_id = bot_id
+
+    def set_text(self, text):
+        self.text = text
+
+    def clear_text(self):
+        self.text = ""
+
+    def add_attachment(self, attachment):
+        try:
+            self.attachments.append(attachment)
+        except AttributeError:
+            self.attachments = [attachment]
+
+    def set_attachment(self, attachments):
+        self.attachments = attachments
+
+    def clear_attachments(self):
+        self.attachments = None
+
+    def clear_all(self):
+        self.clear_text()
+        self.clear_attachments()
+
+    def send_message(self):
+        data = dict()
+        if self.text:
+            data["text"] = self.text
+        if self.attachments:
+            data["attachments"] = self.attachments
+        if data:
+            data["bot_id"] = self.bot_id
+            print(data)
+            requests.post(GROUPME_API_URL, data=data)
+        self.clear_all()  # Don't send the same message twice
+
+
 def untappd(search):
     """Untappd search?"""
     result = ""
@@ -99,63 +143,38 @@ def untappd(search):
     return result
 
 
-def send_message(data=None):
-    """Send a John Cena message to GroupMe"""
-    time.sleep(0.05)
-    if data is None:
-        data = dict()
-    data["bot_id"] = "63747737acc3dbf60d7df729fd"
-    requests.post("https://api.groupme.com/v3/bots/post", data=data)
-
-
 # =============================================================================
 # Search Functions
 # =============================================================================
 def search_gif(query, sender):
     """Search Giphy and return link for search"""
-    data = dict()
-    if not rl.can_send(sender):
-        data["text"] = "You can request another image in {} minutes.".format(rl.time_remaining(sender))
-        return data
-
     try:
         rl.mark_sending(sender)
         time.sleep(0.05)
         query = query.replace(" ", "+")
         res = requests.get(GIF_SEARCH.format(query, GIF_KEY))
         url = res.json()["data"][0]["images"]["fixed_height"]["url"]
-        data["attachments"] = [{"type": "image", "url": url}]
+        CENA.set_text(url)
     except Exception:
-        data["text"] = "Couldn't find a gif, here's a google image instead."
-        send_message(data)
-        data = search_img(query, sender)
-    return data
+        CENA.set_text("Couldn't find a gif, here's a google image instead.")
+        CENA.send_message()
+        search_img(query, sender)
 
 
 def search_img(query, sender):
     """Search for Google images and return"""
-    data = dict()
-    if not rl.can_send(sender):
-        data["text"] = "You can request another image in {} minutes.".format(rl.time_remaining(sender))
-        return data
-
     try:
-        rl.mark_sending(sender)
         service = build("customsearch", "v1", developerKey=IMG_KEY)
-        searcher = service.cse().list(q=query, searchType="image", cx=IMG_CX,
-                                      safe="off")
+        searcher = service.cse().list(q=query, searchType="image", cx=IMG_CX, safe="off")
         res = searcher.execute()
         url = res["items"][0]["link"]
-        data["attachments"] = [{"type": "image", "url": url}]
+        CENA.set_text(url)
     except Exception:
-        data["text"] = "Couldn't find an image"
-        data["attachments"] = [{"type": "image", "url": JOHN_CENA_SAD}]
-    return data
+        CENA.set_text("Couldn't find an image")
 
 
 def maps_search(query, req_url, keys=None, max_res=5, rand=False, attach=False):
     # Basic setup -------------------------------------------------------------
-    data = dict()
     print_keys = {
         "name": "Name",
         "rating": "Rating",
@@ -169,8 +188,8 @@ def maps_search(query, req_url, keys=None, max_res=5, rand=False, attach=False):
         r = requests.get(req_url, verify=False)
         res = r.json()["results"]
     except Exception:
-        data["text"] = "Failed to get Maps info"
-        return data
+        CENA.set_text("Failed to get Maps info")
+        return
 
     # Filter all the data -----------------------------------------------------
     msg = ""
@@ -179,7 +198,6 @@ def maps_search(query, req_url, keys=None, max_res=5, rand=False, attach=False):
         loc = {"type": "location", "lng": None, "lat": None, "name": None}
         msg = "Suggestions for {}\n\n".format(query)
         l = max(map(len, print_keys.values()))
-        data["attachments"] = []
 
         # Perform data selection ----------------------------------------------
         if rand:
@@ -194,7 +212,7 @@ def maps_search(query, req_url, keys=None, max_res=5, rand=False, attach=False):
             loc["lat"] = loc_data["lat"]
             loc["name"] = place["name"]
             if attach:
-                data["attachments"].append(loc)
+                CENA.add_attachment(loc)
             for key in keys:
                 try:
                     print_key = print_keys[key]
@@ -202,68 +220,57 @@ def maps_search(query, req_url, keys=None, max_res=5, rand=False, attach=False):
                 except KeyError:
                     msg += "{:<{}}: {}\n".format(print_key, l, None)
             msg += "\n"
-    data["text"] = msg.strip()
-    return data
+    CENA.set_text(msg.strip())
 
 
 def search_lunch(query, sender):
     query = query + " lunch"
     url = ("{}?query={}&key={}&types=restaurant").format(MAPS_URL, query,
                                                          MAPS_KEY)
-    data = maps_search(query, url, None, 5, True, True)
-    return data
+    maps_search(query, url, None, 5, True, True)
 
 
 def search_maps(query, sender):
     keys = ["name", "formatted_address"]
     url = ("{}?query={}&key={}").format(MAPS_URL, query, MAPS_KEY)
-    data = maps_search(query, url, keys, 5, False, False)
-    return data
+    maps_search(query, url, keys, 5, False, False)
 
 
 def python_eval(query, sender):
     """Call a local python command/function and return stdout"""
-    data = dict()
     cmd = "python -c \"{}\"".format(query)
     time.sleep(0.05)
     try:
         proc = subprocess.Popen(cmd, stdin=None, stdout=subprocess.PIPE,
                                 shell=True)
         stdout = proc.communicate()[0]
-        data["text"] = ">>> {}".format(str(stdout)[:995])
+        CENA.set_text(">>> {}".format(str(stdout)[:995]))
     except Exception as e:
-        data["text"] = "Error: {}".format(str(e))
-    return data
+        CENA.set_text("Error: {}".format(str(e)))
 
 
 def search_wolfram(query, sender):
     """Search wolfram alpha for info"""
-    data = dict()
     try:
         client = wolframalpha.Client(WOLF_KEY)
         res = client.query(query)
-        data["text"] = "\n".join([pod.text for pod in res.pods[:2]])
+        CENA.set_text("\n".join([pod.text for pod in res.pods[:2]]))
     except Exception:
-        data["text"] = "Error processing WolframAlpha search"
-    return data
+        CENA.set_text("Error processing WolframAlpha search")
 
 
 def search_wiki(query, sender):
     """Search wikipedia for subject page"""
-    data = dict()
     try:
         page = wikipedia.page(query)
-        data["text"] = page.summary.split("\n")[0][:900]
+        CENA.set_text(page.summary.split("\n")[0][:900])
     except Exception:
         other_pages = "\n".join(wikipedia.search(query)[:5])
-        data["text"] = ("Couldn't find a match on wikipedia. "
-                        "Could it be one of these?\n{}".format(other_pages))
-    return data
+        CENA.set_text("Couldn't find a match on wikipedia. Could it be one of these?\n{}".format(other_pages))
 
 
 def search_yt(query, sender):
     """Perform youtube video search"""
-    data = dict()
     try:
         formatted_query = query.replace(" ", "+")
         x = requests.get(YT_REQ.format(formatted_query, YT_KEY))
@@ -273,10 +280,9 @@ def search_yt(query, sender):
             if yt_id["kind"].lower() in ("youtube#video", ):
                 videoid = yt_id["videoId"]
                 break
-        data["text"] = "https://www.youtube.com/watch?v=" + videoid
+        CENA.set_text("https://www.youtube.com/watch?v={}".format(videoid))
     except Exception:
-        data["text"] = "Couldn't find a video on youtube"
-    return data
+        CENA.set_text("Couldn't find a video on youtube")
 
 
 def show_help(query, sender):
@@ -284,7 +290,8 @@ def show_help(query, sender):
     l = max(map(len, fns)) + 4
     fnhelp = "\n".join(["  {:<{}}{}".format(f, l, SEARCHES[f]["help"])
                         for f in fns])
-    return __doc__.format(fnhelp)
+    CENA.set_text(__doc__.format(fnhelp))
+
 
 def redeploy(query, sender):
     subprocess.call("git pull origin master && supervisorctl restart johncena", shell=True)
@@ -318,23 +325,25 @@ def bot(params=None):
 #	except Exception:
 #	    pass
 #
+    # Gather parameters (None check is for dev purposes)
     if params is None:
         params = json.loads(request.params.keys()[0])
+
     text_in = params["text"].split()
     try:
         s_type, query = text_in[0].lower(), " ".join(text_in[1:])
     except IndexError as e:
         return  # Not enough inputs to perform a search
 
+    # Attempt to call function & send CENA's message
+    CENA.clear_all()
     try:
-        data = SEARCHES[s_type]["fn"](query, params["sender_id"])
-        send_message(data)
+        SEARCHES[s_type]["fn"](query, params["sender_id"])
     except (KeyError, NameError) as e:
-        pass  # Ignore invalid keys to the lookup dict
+        CENA.clear_all()
     except Exception as e:
-        data = {"text": "WTF: {}".format(str(e))}
-        data["attachments"] = [{"type": "image", "url": JOHN_CENA_WTF}]
-        send_message(data)
+        CENA.set_text("WTF: {}".format(str(e)))
+    CENA.send_message()
 
 
 @route('/')
@@ -384,12 +393,16 @@ SEARCHES = {
         "help": "Redeploy John Cena (dev purposes only)",
     },
 }
+
+CENA = GroupmeMessage()
+
+
 # =============================================================================
 # Entry Code
 # =============================================================================
 if __name__ == "__main__":
-    startup = {"text": "John Cena ACTIVATE!", "attachments": [{"type": "image", "url": JOHN_CENA_ACTIVATE}]}
-    send_message(startup)
+    CENA.set_text(JOHN_CENA_ACTIVATE)
+    CENA.send_message()
     run(host='0.0.0.0', port=80)
 
 app = default_app()
